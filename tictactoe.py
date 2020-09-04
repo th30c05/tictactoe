474a6b135347286dffcdf03c2e29198c31ece519
th30c05 <arnaufugarolas@gmail.com>
2020-09-04 16:25:13 +0200

    Fix result function errors.

--- tictactoe.py
+++ tictactoe.py
@@ -71,21 +71,9 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    i = action[0]
-    j = action[1]
-    goal_position = ((i*3)+j)
-    actual_position = 0
     result_board = board
-    finish = False
 
-    while not finish:
-        if actual_position == goal_position:
-            result_board[goal_position] = player(board)
-            finish = True
-        else:
-            finish = False
-
-        actual_position += 1
+    (result_board[action[0]])[action[1]] = player(board)
 
     return result_board
 
